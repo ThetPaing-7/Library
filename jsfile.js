535c1ff9b10da40889a0f5ed75e156552e85c16c
ThetPaing-7 <thatpainlin1999@gmail.com>
2024-10-13 11:41:52 +0630

    Done functionality

--- jsfile.js
+++ jsfile.js
@@ -3,10 +3,11 @@ const holder = document.querySelector(".container");
 
 const myLibrary = [];
 
-function Book(year, author, title){
+function Book(year, author, title,read = false){
     this.year = year;
     this.author = author;
     this.title = title;
+    this.read = read;
 }
 
 function addBookToLibrary(year,author,title){
@@ -45,6 +46,7 @@ addButton.addEventListener("click",(event)=>{
     
     if(!isDuplicate){
         addBookToLibrary(year.value,author.value,title.value);
+        display();
     }else{
         alert("The book is already exists in the library")
     }
@@ -71,7 +73,12 @@ function display(){
     //take container
     const container = document.querySelector(".book-container");
 
-    myLibrary.forEach((book) =>{
+    // Clear previous books
+    container.innerHTML = '';
+
+    myLibrary.forEach((book,index) =>{
+
+        
         const bookCard = document.createElement("div");
         bookCard.classList.add("book-card");
 
@@ -93,8 +100,41 @@ function display(){
         year.textContent = `Year: ${book.year}`
         bookCard.append(year)
 
+        // Adding delete button
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-button");
+        deleteButton.textContent = "Remove";
+
+        // Take the index of the book
+        deleteButton.setAttribute('data-index',index);
+
+        // Add event listen to delete
+        deleteButton.addEventListener("click",handleDelete);
+        bookCard.append(deleteButton);
+
+        // Add read button on each book
+        const readButton = document.createElement("button")
+        readButton.classList.add("read-button")
+        readButton.textContent = book.read ? "Read" : "Not Read";
+
+        readButton.style.background = book.read ? "green" : "red";
+
+        readButton.addEventListener("click",()=>{
+            book.read = !book.read;
+            readButton.textContent = book.read ? "Read" : "Not Read";
+            readButton.style.background = book.read ? "green" : "red";
+        })
+        bookCard.append(readButton);
+
         container.append(bookCard);
     })
 
     holder.append(container);
+}
+
+// To handle delete action
+function handleDelete(event){
+    const index = event.target.getAttribute('data-index');
+    myLibrary.splice(index,1);
+    display();
 }
